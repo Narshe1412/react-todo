dfab3404a7cd60aa249967f04d61a9ba9edbf214
Manuel <manuel1412@gmail.com>
2017-03-27 16:33:38 +0100

    first commit

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+//load foundation
+$(document).foundation();
+//styles
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+<p>Boilerplate 3</p>,
+    document.getElementById('app')
+)
