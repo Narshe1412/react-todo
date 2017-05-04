87579111c0e8785b34c3ebccfe22f042c9970144
Manuel <manuel1412@gmail.com>
2017-05-04 09:30:28 +0100

    half of reducers already done

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
         alias: {
             //Component: './components/Name.jsk
             applicationStyles: 'app/styles/app.scss',
-            actions: 'app/actions/actions.jsx'
+            actions: 'app/actions/actions.jsx',
+            reducers: 'app/reducers/reducers.jsx'
         },
         extensions: ['', '.js', '.jsx']
     },
