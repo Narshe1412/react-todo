6f739ff01529c849a061dde1e1513f4b5ab1c8ef
Manuel <manuel1412@gmail.com>
2017-05-04 12:35:43 +0100

    store added

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,17 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(()=>{
+    console.log("new state", store.getState())
+})
+
+store.dispatch(actions.addTodo('clean a yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 //load foundation
 $(document).foundation();
 //styles
